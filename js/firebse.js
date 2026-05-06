669cd7cdc8cf1d7052a9391604faadf06f36984d
beneditosa-crypto <benedito.sa@gmail.com>
2026-05-06 08:56:45 -0300

    Update firebse.js

--- js/firebse.js
+++ js/firebse.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9-eYhr2Bdadd4OWD17zIRszsz3LrxeBc",
+  authDomain: "clube-da-caminhonete-be770.firebaseapp.com",
+  projectId: "clube-da-caminhonete-be770",
+  storageBucket: "clube-da-caminhonete-be770.firebasestorage.app",
+  messagingSenderId: "559157035885",
+  appId: "1:559157035885:web:8d3c4c8d7c7c5f7b3b2a91"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
